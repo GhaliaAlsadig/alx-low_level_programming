6e8b9297e303b12983ebb65e3c4f4a4021572363
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-13 17:30:05 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,2 @@
-#include <stidio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-
-return(0);
-}
+#!/bin/bash
+Programming is like building a multilingual puzzle
