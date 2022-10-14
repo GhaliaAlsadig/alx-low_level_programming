b1106cd42edb603fc6505ac65b469c313b7ed885
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-13 18:57:48 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
+description:' '
  *  * main - Entry point
+description:' '
+description:' '
+description:' '
  *    * Return: Always 0 (Success)
+description:' '
      */
 int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
