50911089f921857876b53a6bfef4132864cfa089
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-19 18:08:25 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * function - print_alphabet_x10
+ * description printx10
+ */
+void print_alphabet_x10(void)
+{
+	for (int y = 0 ; y < 10 ; y++)
+	{
+		char x = 'a';
+
+		while (x <= 'z')
+		{
+			_putchar('x');
+			_putchar('\n';
+			x++;
+			}
+	}
+	return (0);
+}
