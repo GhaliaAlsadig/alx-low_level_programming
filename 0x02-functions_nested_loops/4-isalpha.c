44fa47dd35d2f9c50dc0a609fdd1616ffccf62b8
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-19 19:35:45 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * description
+ * _isalpha checks for char alphabet
+ * @c: is a char to br checked
+ *
+ * return:1 if char is a letter upper or lower , 0: otherwise
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= z || c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
