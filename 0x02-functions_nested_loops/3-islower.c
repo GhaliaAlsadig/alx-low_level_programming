be6ee076cf6545bfe17e7fde9c7ef44930857365
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-19 18:42:21 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function - main
+ * description print islower
+ */
+int _islower(int c)
+{
+	if ('c' >= 'a' && 'c' <= 'z')
+	{
+		_putchar(1);
+	} else
+	{
+		return (0);
+	}
+}
