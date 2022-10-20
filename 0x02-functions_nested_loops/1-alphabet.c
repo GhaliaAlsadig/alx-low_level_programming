9a45c41ff6adce3ef63b76e30e0f8274382e8b5d
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-19 17:29:53 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * function - main
+ * description print alphapet
+ */
+int main(void)
+{
+	int x = a;
+
+	while (w <= z)
+	{
+		_putchar('x');
+		_putchar('\n');
+		x++;
+	}
+	return (0);
+}
