a1952128d60c112cf29bafbd48c43c0aa749af7d
GhaliaAlsadig <ghaliaalsadig27@gmail.com>
2022-10-28 18:16:43 -0700

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,26 @@
 #include "main.h"
 /**
- * _strncpy - copys a string with n
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
- * return: dest
+ * _strncpy - copies a string to another string
+ *
+ * @dest: the buffer
+ * @src: the string to be copied
+ * @n: the number of bytes to include in @dest
+ *
+ * Return: pointer to the resulting string @dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int len1; /* string length for src */
+	int len2; /* string length for dest */
+	int i;
 
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
+	while (src[len1] != '\0')
+		len1++;
+	while (dest[len2] != '\0')
+		len2++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,20 +3,18 @@
  * _strcmp - compares two strinhs values
  * @s1: string 1
  * @s2: string 2
- * return: the diffrence in s1 and s2
+ * return: difference in length
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i])
 	{
-		if (s1[i] != s2[i])
-		{
+		if (s1[i] == '\0')
 			return (s1[i] - s2[i]);
-		}
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
